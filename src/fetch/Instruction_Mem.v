0e9128309bbb9921398524bdecaa8702e4a15ee3
Mahela Ekanayake <mahelaekanayake.aurelius@gmail.com>
2026-03-25 12:05:42 +0530

    Update instruction memory file path to 'test/fibonacci.hex'

--- src/fetch/Instruction_Mem.v
+++ src/fetch/Instruction_Mem.v
@@ -8,7 +8,7 @@ module Instruction_Mem (
 
     // Load instructions from hex file at the start
     initial begin
-        $readmemh("fibonacci.hex", I_Mem); // Ensure this file exists in simulation directory
+        $readmemh("test/fibonacci.hex", I_Mem); // Ensure this file exists in simulation directory
     end
 
     always @(negedge clk) begin
